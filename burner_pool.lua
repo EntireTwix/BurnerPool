50b4ac14ec69a58556c196e5c3974ee63ceca282
William <qualitycontentquestionmark@gmail.com>
2023-06-10 01:09:06 -0700

    :fire: didnt use `spairs`

--- burner_pool.lua
+++ burner_pool.lua
@@ -2,29 +2,6 @@ local ccash = require("ccash.api")
 
 local module = {}
 
-function spairs(t, order)
-    -- collect the keys
-    local keys = {}
-    for k in pairs(t) do keys[#keys+1] = k end
-
-    -- if order function given, sort by it by passing the table and keys a, b,
-    -- otherwise just sort the keys 
-    if order then
-        table.sort(keys, function(a,b) return order(t, a, b) end)
-    else
-        table.sort(keys)
-    end
-
-    -- return the iterator function
-    local i = 0
-    return function()
-        i = i + 1
-        if keys[i] then
-            return keys[i], t[keys[i]]
-        end
-    end
-end
-
 function make_burner()
     local name = tostring(math.random(10^2, 10^16 - 1))
     local pass = tostring(math.random(10^8, 10^9 - 1))
